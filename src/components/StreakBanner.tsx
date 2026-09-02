a73192815709183865ee7d619cfdc3498154e3cc
Daniel Pravutiner <62667601+danprav04@users.noreply.github.com>
2026-09-02 05:18:46 +0300

    Update StreakBanner.tsx

--- src/components/StreakBanner.tsx
+++ src/components/StreakBanner.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, StyleSheet, Animated } from 'react-native';
 import * as Haptics from 'expo-haptics';
 
